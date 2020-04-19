2cf549fd5aae9d96b70f0f9bd77ebd7a11d75a26
Gabriel F. Vilar <cogumm@gmail.com>
2020-04-19 19:44:54 -0300

    Variável Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require("dotenv/config");
 
 module.exports = {
   dialect: "postgres",
-  host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
