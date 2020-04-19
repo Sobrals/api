918ffe30bf8f0b66e465e68d7586adbd6c45ddd3
Luciano Sobral <sobral.luciano@gmail.com>
2020-04-18 21:52:54 -0400

    BrokerController Implemented

---
+++ src/controllers/BrokerController.js
@@ -0,0 +1,48 @@
+const Broker = require('../models/Broker')
+const Yup = require('yup');
+
+
+class BrokerController {
+
+    async store(request, response) {
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string()
+          .email()
+          .required(),
+        password: Yup.string()
+          .min(6)
+          .required(),
+        creci: Yup.string(),
+        phone: Yup.string()
+      });
+
+      const schemaValid = await schema.isValid(request.body);
+
+      if (!schemaValid) {
+        return response.status(401).json({ error: 'Validation Fails!' });
+      }
+
+      const { name, email, password, creci, phone } = request.body;
+
+      const broker = await Broker.findOne({ where: { email } });
+
+
+      if (broker) {
+        return response.status(400).json({ error: 'Broker already exist!' });
+      }
+
+      const { id } = await Broker.create({
+        name,
+        email,
+        password,
+        creci,
+        phone
+      });
+
+      return response.status(201).json({ id, name, email, phone, creci });
+    }
+
+}
+
+module.exports = new BrokerController()
